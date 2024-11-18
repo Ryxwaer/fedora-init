1955228915ce857169effda3aa755bde169985db
Ryxwaer <ryxwaer@gmail.com>
2024-11-18 22:16:53 +0100

    fedora frog

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    fastfetch --logo-type chafa --logo-width 34 --logo-height 16 --logo  "/home/ryxwaer/.config/fastfetch/fedora.png"
+    fastfetch --logo-type chafa --logo-width 36 --logo-height 16 --logo  "/home/ryxwaer/.config/fastfetch/fedora.png"
 end
 
 if status is-interactive
